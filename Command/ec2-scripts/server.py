e2fbf70d52b59c407fa386268a120b23d3cd0902
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-05-23 15:09:41 +0100

    Update server.py

--- Command/ec2-scripts/server.py
+++ Command/ec2-scripts/server.py
@@ -12,14 +12,13 @@ import os.path
 
 def main():
 
-    server_name = '18.132.60.200'  # public ipv4 of ec2
+    server_name = '18.169.131.196'  # public ipv4 of ec2
     server_port = 12000                        
     #create a TCP client socket
     ec2_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Bind board 1 socket to port 11000
-    ec2_client_socket.bind(('', 16090))  # Board 2: 16000
+   
 
-    print("Running UDP client for board 1...")
+    print("Running TCP server on EC2 instance")
     #user presses button to join game
     # Add user/player to database
     msg = "board1"
