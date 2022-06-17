a113971eda27a926ff642201c23b16d11c00baf5
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-06-17 16:17:00 +0100

    y

---
+++ Command/src/automate.py
@@ -0,0 +1,27 @@
+
+
+poi = [ [] ]  # Points of interest
+# Add corners of arena to 
+poi.append([20,20])
+poi.append([220,340])
+poi.append([20,340])
+poi.append([220,20])
+
+def automate():
+
+    return (command)
+
+
+
+def add_POI(x, y):
+    poi.append([x,y])
+    print("POI added")
+
+
+
+
+def drive( x, y ):      #takes in current position of rover
+    
+    while 0<y<360 and 0<x<240:
+        #make drive things
+        
--- Command/src/tcpserver.py
+++ Command/src/tcpserver.py
@@ -1,111 +1,235 @@
+from http import server
+from re import I
 import socket
+import threading
 import yap
-import RadicalProc
+import time
+import automate 
+
+
+# Create a TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('192.168.43.192', 15000)
+# print('starting up on port ' + server_address[0] + server_address[1])
+server.bind(server_address)
+# Listen for incoming connections
+server.listen(1)
+
+clients = []
+nicknames = []
+
 
 Longitina =[]
 Latina =[]
 Alien =[]
-n=1
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sav_dist=0
+x=0
+y=0
+sav_loc =[x,y]
+sav_Rangle=0
 
+def rov_decode(message1):
+    message1=int.from_bytes(message1, "big", signed="true")
+    message1=str(message1)
+    return message1
 
-# Bind the socket to the port
-server_address = ('localhost', 16000)
-# print('starting up on port ' + server_address[0] + server_address[1])
-sock.bind(server_address)
-
-
-# Listen for incoming connections
-sock.listen()
-
-
-while True:
-    # Wait for a connection
-    print ('waiting for a connection')
-    
-    connection, client_address = sock.accept() #connect controller first
-
-#try converting above into try and except statements?
+def broadcast(message):
 
     try:
-        print('connection ')
+        for client in clients:
+            message = message.encode('ascii')
+            client.send(message)
+    except:
+        #need to fix it, summit bout a while loop and flags. 
+        client, address = server.accept()
+        print("connected to " + str(address))
 
-        while True:
+        #client.send('Nick'.encode('ascii'))
+        nickname  = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
 
-            contr_data = connection.recv(48)
+def handle(client):
+    i = 0
+    while True:
+        try:
+            #print("in handle")
+            message = client.recv(1024)
 
-            contr_data = contr_data.decode()
-            print('contr data: ' + contr_data)
+            message = message.decode("ascii")
+            print(message)
+            message=str(message)
+            opcode = message[0:3]
 
-
-            opcodecontr = contr_data[0:3]
-
-
-            if opcodecontr == 'POS':
-                x=int(contr_data[3:6])
-                # print(x)
-                y=int(contr_data[6:9])
-                #print(y)
-                Rangle=int(contr_data[9:12])
-                #print(Aangle)
-                yap.alien(x,y,Rangle,'#ffffff',0,0,Longitina,Latina,Alien,n)
-
-
-            if opcodecontr == 'IDA':
-                #level="IDA"+"010"+"024"+"045"+"fF00ff"+"001"+"28"+"28"+"1"
-                x=int(contr_data[3:6])
-                # print(x)
-                y=int(contr_data[6:9])
-                #print(y)
-                Aangle=int(contr_data[9:12])
-                #print(Aangle)
-                colour='#'+contr_data[12:18]
-                #print(colour)
-                Rangle=int(contr_data[18:21])
-                #print(Rangle)
-                dist=int(contr_data[21:25])
-                dist=float(dist/100)
-                #print(dist)
-                yap.alien(x,y,Rangle,colour,Aangle,dist,Longitina,Latina,Alien,n)
-                #yap.draw(Longitina, Latina, Alien)
-                #print(Longitina[0])
-                # f = open("Command/src/components/Map.js", "a")
-                # f.close()
-                # f.close()
-
-            if opcodecontr == 'RAD':
-                print( "test signal")
-                signal=input()
-                RadicalProc.Radarloc(Longitina, Latina, Alien,signal)
+            if opcode == "MOV" or "MOD":
+                # broadcast(message)
+                print(message)
+                client = clients[1]
+                client.send(message)
 
 
+            if opcode == "vof" or "rof":  #if vision or radar is toggled OFF from app
+                # broadcast(message)
+                print(message)
+                client = clients[1]
+                client.send(message)
 
-            if opcodecontr == 'BAT':
+            if opcode == "von" or "ron":  #if vision or radar is toggled ON from app
+                # broadcast(message)
+                print(message)
+                client = clients[1]
+                client.send(message)
+
+
+            if message == "POS":
+                print("in pos")
+                time.sleep(1)
+                message1 = client.recv(1024)
+                message1=int.from_bytes(message1, "big", signed="true")
+                message1=str(message1)
+                print('traveled: '+ message1)
+                Rangle=message1
+                time.sleep(1)
+                message2 = client.recv(1024)
+                message2=int.from_bytes(message2, "big", signed="true")
+                message2=str(message2)
+                print('angle:'+message2)
+                dist=message2-sav_dist
+                # loc=yap.calc_loc(Rangle,sav_dist,dist,sav_loc)
+                # x=loc[0]
+                # y=loc[1]
+                # print('x:'+x)
+                # print('y:'+y)
+                if (Rangle!=sav_Rangle):
+                    sav_loc=loc
+                    sav_Rangle=Rangle
+                    sav_dist=dist
                 
-                level_val = contr_data[3:6]
+                message4 = client.recv(1024)
+                message4=int.from_bytes(message4, "big", signed="true")
+                message4=str(message4)
+            if opcode== "IDA":
+                print ("ida")
+                message1 = client.recv(1024)
+                message1=int.from_bytes(message1, "big", signed="true")
+                message1=str(message1)
+                print("dist:" + message1)
+                colour='#FF0000'
+                yap.alien(x,y,Rangle,colour,dist,Longitina,Latina,Alien)
 
-                print('recieved battery level')
-                
-                infile = open('components/Level.js','r+')
-                content = infile.readlines() #reads line by line and out puts a list of each line
-                content[3] = 'let inputval = ' + level_val + ';' #replaces content of the 2nd line (index 1)
-                infile.close()
-                infile = open('components/Level.js', 'w') #clears content of file. 
-                infile.close
-                infile = open('components/Level.js', 'r+')
-                for item in content: #rewrites file content from list 
-                    infile.write("%s" % item)
-                infile.close()
-
+            if opcode=="RAD":
+                message4 = client.recv(1024)
+                message4=int.from_bytes(message4, "big", signed="true")
+                message4=str(message4)
+                print("dist:" + message4)
+                yap.alien(x,y,Rangle,'#000000',dist,Longitina,Latina,Alien)
             
-                # connection.sendall('data')
 
+            yap.alien(x,y,Rangle,'#ffffff',0,0,Longitina,Latina,Alien)
+                
 
+                
+            #19, 16, 13 circ, 22 degree from center
+
+            time.sleep(1)
+            #     # x=int(message[3:6])
+            #     # # print(x)
+            #     # y=int(message[6:9])
+            #     # #print(y)
+            #     # Rangle=int(message[9:12])
+            #     # #print(Aangle)
+            #     # yap.alien(x,y,Rangle,'#ffffff',0,0,Longitina,Latina,Alien)
+           
+            # if opcode == "IDA":a
+            #    #level="IDA"+"010"+"024"+"045"+"ff00ff"+"001"+"28"+"28"+"1"
+            #     x=int(message[3:6])
+            #     # print(x)
+            #     y=int(message[6:9])
+            #     #print(y)
+            #     Aangle=int(message[9:12])
+            #     #print(Aangle)
+            #     colour='#'+message[12:18]
+            #     #print(colour)
+            #     Rangle=int(message[18:21])
+            #     #print(Rangle)
+            #     dist=int(message[21:25])
+            #     dist=float(dist/100)
+            #     #print(dist)
+            #     yap.alien(x,y,Rangle,colour,Aangle,dist,Longitina,Latina,Alien)
+            #     #yap.draw(Longitina, Latina, Alien)
+            #     #print(Longitina[0])
+            #     # f = open("Command/src/components/Map.js", "a")
+            #     # f.close()
+            #     # f.close()
+            
+            # if opcode == 'BAT':
+                
+            #     level_val = message[3:6]
+
+            #     print("recieved battery level")
+                
+            #     infile = open('components/Level.js','r+')
+            #     content = infile.readlines() #reads line by line and out puts a list of each line
+            #     content[3] = 'let inputval = ' + level_val + ';' #replaces content of the 2nd line (index 1)
+            #     infile.close()
+            #     infile = open('components/Level.js', 'w') #clears content of file. 
+            #     infile.close
+            #     infile = open('components/Level.js', 'r+')
+            #     for item in content: #rewrites file content from list 
+            #         infile.write("%s" % item)
+            #     infile.close()
             # else:
-            #    #print('no more data')
-            #      break
+            exit (1)
             
-    finally:
-        # Clean up the connection
-        connection.close()
+
+
+        except:
+            if i %5==1:
+                msg = str("4")
+                broadcast(msg)
+            if i %5==2:
+                msg = str("1")
+                broadcast(msg)
+            if i %5==3:
+                msg = str("2")
+                broadcast(msg)
+            if i %5==4:
+                msg = str("3")
+                broadcast(msg)
+            else:
+                broadcast("0")
+            i = i+1
+
+            
+
+            # print('your mom')
+            # index = clients.index(client)
+            # clients.remove(client)
+            # client.close()
+            # nickname = nicknames[index]
+            # # broadcast((nickname + "left the chat!").encode('ascii'))
+            # nicknames.remove(nickname)
+            # break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print("connected to " + str(address))
+
+        #client.send('Nick'.encode('ascii'))
+        nickname  = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print("Nickname of client is " + nickname)
+        # broadcast((nickname +  "joined the chat!").encode('ascii'))
+        # client.send("connected to server!".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server is listening...")
+receive()
